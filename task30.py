3bb0dfd2a3e999598fd873059f06c76c06609354
annashteynert <132136488+annashteynert@users.noreply.github.com>
2023-05-21 01:02:43 +0700

    Add files via upload

---
+++ task30.py
@@ -0,0 +1,10 @@
+string = input("Введите строку: ")
+counter = 0
+counter1 = 0
+for i in string:
+ if i.lower() in "aeiou":
+  counter += 1
+ elif i.isalpha():
+  counter1 += 1
+print("Количество гласных букв:", counter)
+print("Количество согласных букв:", counter1)
