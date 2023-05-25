655a135af1e090fe87a16785dbe804adb57358f0
annashteynert <132136488+annashteynert@users.noreply.github.com>
2023-05-25 21:16:34 +0700

    Update task30.py

--- task30.py
+++ task30.py
@@ -2,7 +2,7 @@ string = input("Введите строку: ")
 counter = 0
 counter1 = 0
 for i in string:
- if i.lower() in "aeiou":
+ if i.lower() in "аеёиоуыэюя":
   counter += 1
  elif i.isalpha():
   counter1 += 1
