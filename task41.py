86923bb1a4a9fc15416876e634377b18c0a430ba
annashteynert <132136488+annashteynert@users.noreply.github.com>
2023-05-28 00:16:55 +0700

    Update task41.py

--- task41.py
+++ task41.py
@@ -1,4 +1,11 @@
+import os
 
-from pathlib import Path
-
-Path("new_file.txt").write_text("Hello, world!")
+directory = 'my_folder'
+if not os.path.exists(directory):
+  try:
+   os.mkdir(directory)
+   print(f"Создание  '{directory}' прошло успешно ")
+  except OSError:
+   print(f"Директория '{directory}' не создана")
+else:
+   print(f"Директория '{directory}' уже есть")
